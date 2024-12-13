eb2b7274e6ca725c6acb61459f2350d4fcc0c363
Carmani H. Jackson <charrisjackson422@gmail.com>
2024-12-13 18:12:49 -0500

    ?

--- notes-app/utils.js
+++
@@ -1,9 +0,0 @@
-console.log('utils.js');
-const name = 'Mike'
-const add= function (a,b){
-return a + b
-}
-const subtract = function (x,y){
-    return y-x
-}
-module.exports = add  ;     //defines what this file should share with other files
